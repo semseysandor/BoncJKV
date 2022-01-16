898926afa804830a13fa286282f5021928669814
Sandor Semsey <semseysandor@gmail.com>
2022-01-16 01:32:22 +0100

    version

--- Source/Forms/About/About.Designer.vb
+++ Source/Forms/About/About.Designer.vb
@@ -30,110 +30,110 @@ Partial Class About
     Me.Label4 = New System.Windows.Forms.Label()
     Me.GroupBox1 = New System.Windows.Forms.GroupBox()
     Me.Button1 = New System.Windows.Forms.Button()
-    CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
-    Me.GroupBox1.SuspendLayout()
-    Me.SuspendLayout()
-    '
-    'PictureBox1
-    '
-    Me.PictureBox1.Image = Global.BoncJKV.My.Resources.Resources.boncjkv
-    Me.PictureBox1.Location = New System.Drawing.Point(12, 15)
-    Me.PictureBox1.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
-    Me.PictureBox1.Name = "PictureBox1"
-    Me.PictureBox1.Size = New System.Drawing.Size(47, 46)
-    Me.PictureBox1.TabIndex = 0
-    Me.PictureBox1.TabStop = False
-    '
-    'Label1
-    '
-    Me.Label1.AutoSize = True
-    Me.Label1.Font = New System.Drawing.Font("Bahnschrift Light SemiCondensed", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
-    Me.Label1.Location = New System.Drawing.Point(6, 39)
-    Me.Label1.Name = "Label1"
-    Me.Label1.Size = New System.Drawing.Size(113, 21)
-    Me.Label1.TabIndex = 1
-    Me.Label1.Text = "Semsey Sándor"
-    '
-    'Label2
-    '
-    Me.Label2.AutoSize = True
-    Me.Label2.Font = New System.Drawing.Font("Bahnschrift Light SemiCondensed", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
-    Me.Label2.Location = New System.Drawing.Point(6, 64)
-    Me.Label2.Name = "Label2"
-    Me.Label2.Size = New System.Drawing.Size(109, 21)
-    Me.Label2.TabIndex = 2
-    Me.Label2.Text = "Copyright 2020"
-    '
-    'Label3
-    '
-    Me.Label3.AutoSize = True
-    Me.Label3.Font = New System.Drawing.Font("Bahnschrift Light SemiCondensed", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
-    Me.Label3.Location = New System.Drawing.Point(6, 89)
-    Me.Label3.Name = "Label3"
-    Me.Label3.Size = New System.Drawing.Size(45, 21)
-    Me.Label3.TabIndex = 3
-    Me.Label3.Text = "v1.0.0"
-    '
-    'Label4
-    '
-    Me.Label4.AutoSize = True
-    Me.Label4.Font = New System.Drawing.Font("Bahnschrift Light SemiCondensed", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
-    Me.Label4.Location = New System.Drawing.Point(6, 18)
-    Me.Label4.Name = "Label4"
-    Me.Label4.Size = New System.Drawing.Size(66, 21)
-    Me.Label4.TabIndex = 4
-    Me.Label4.Text = "BoncJKV"
-    '
-    'GroupBox1
-    '
-    Me.GroupBox1.Controls.Add(Me.Label4)
-    Me.GroupBox1.Controls.Add(Me.Label3)
-    Me.GroupBox1.Controls.Add(Me.Label1)
-    Me.GroupBox1.Controls.Add(Me.Label2)
-    Me.GroupBox1.Location = New System.Drawing.Point(65, 4)
-    Me.GroupBox1.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
-    Me.GroupBox1.Name = "GroupBox1"
-    Me.GroupBox1.Padding = New System.Windows.Forms.Padding(3, 4, 3, 4)
-    Me.GroupBox1.Size = New System.Drawing.Size(130, 118)
-    Me.GroupBox1.TabIndex = 5
-    Me.GroupBox1.TabStop = False
-    '
-    'Button1
-    '
-    Me.Button1.Font = New System.Drawing.Font("Bahnschrift", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
-    Me.Button1.Location = New System.Drawing.Point(12, 126)
-    Me.Button1.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
-    Me.Button1.Name = "Button1"
-    Me.Button1.Size = New System.Drawing.Size(183, 46)
-    Me.Button1.TabIndex = 6
-    Me.Button1.Text = "OK"
-    Me.Button1.UseVisualStyleBackColor = True
-    '
-    'About
-    '
-    Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 19.0!)
-    Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-    Me.ClientSize = New System.Drawing.Size(202, 184)
-    Me.Controls.Add(Me.Button1)
-    Me.Controls.Add(Me.GroupBox1)
-    Me.Controls.Add(Me.PictureBox1)
-    Me.Font = New System.Drawing.Font("Bahnschrift Light SemiCondensed", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
-    Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
-    Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
-    Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
-    Me.MaximizeBox = False
-    Me.MinimizeBox = False
-    Me.Name = "About"
-    Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
-    Me.Text = "BoncJKV - Névjegy"
-    CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
-    Me.GroupBox1.ResumeLayout(False)
-    Me.GroupBox1.PerformLayout()
-    Me.ResumeLayout(False)
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.BoncJKV.My.Resources.Resources.boncjkv
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 15)
+        Me.PictureBox1.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(47, 46)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Bahnschrift Light SemiCondensed", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.Label1.Location = New System.Drawing.Point(6, 39)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(113, 21)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Semsey Sándor"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Bahnschrift Light SemiCondensed", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.Label2.Location = New System.Drawing.Point(6, 64)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(109, 21)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Copyright 2020"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Bahnschrift Light SemiCondensed", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.Label3.Location = New System.Drawing.Point(6, 89)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(42, 21)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "v1.1.0"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Bahnschrift Light SemiCondensed", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.Label4.Location = New System.Drawing.Point(6, 18)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(66, 21)
+        Me.Label4.TabIndex = 4
+        Me.Label4.Text = "BoncJKV"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Location = New System.Drawing.Point(65, 4)
+        Me.GroupBox1.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Padding = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.GroupBox1.Size = New System.Drawing.Size(130, 118)
+        Me.GroupBox1.TabIndex = 5
+        Me.GroupBox1.TabStop = False
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Bahnschrift", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.Button1.Location = New System.Drawing.Point(12, 126)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(183, 46)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "OK"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'About
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 19.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(202, 184)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Font = New System.Drawing.Font("Bahnschrift Light SemiCondensed", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "About"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "BoncJKV - Névjegy"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
 
-  End Sub
+    End Sub
 
-  Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox1 As PictureBox
 	Friend WithEvents Label1 As Label
 	Friend WithEvents Label2 As Label
 	Friend WithEvents Label3 As Label
